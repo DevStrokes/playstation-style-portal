594ee7b584cc0901117b3b8640b7d81ad6cb0d6e
DevStrokes <110514461+DevStrokes@users.noreply.github.com>
2026-08-03 22:57:59 +0200

    Delete src/client.tsx

--- src/client.tsx
+++
@@ -1,7 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/start'
-import { getRouter } from './router'
-
-const router = getRouter()
-
-hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
