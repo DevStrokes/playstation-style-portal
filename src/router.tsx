e6bae05bfe6c946d1e66fc985a6717eb28dea638
DevStrokes <110514461+DevStrokes@users.noreply.github.com>
2026-08-03 22:55:26 +0200

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,13 @@
-import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
+import { getRouter } from './router'
+import './styles.css'
 
-export const getRouter = () => {
-  const queryClient = new QueryClient();
+const router = getRouter()
 
-  const router = createRouter({
-    routeTree,
-    context: { queryClient },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  });
-
-  return router;
-};
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
